0c3c68362388088e947a37fb48f086dd9122142f
gserifi <gent.serifi@gmail.com>
2024-02-11 21:24:32 +0100

    cleaned up camera code

---
+++ src/camera.rs
@@ -0,0 +1,93 @@
+use crate::color::{Color, ColorWriter};
+use crate::hittable::{HitRecord, Hittable};
+use crate::interval::Interval;
+use crate::ray::Ray;
+use crate::vec3::{Point3, Vec3, XYZAccessor};
+
+pub struct Camera {
+    pub aspect_ratio: f64,
+    pub image_width: i32,
+
+    // Derived
+    image_height: i32,
+    origin: Point3,
+    pixel00_loc: Point3,
+    pixel_delta_u: Vec3,
+    pixel_delta_v: Vec3,
+}
+
+impl Camera {
+    pub fn new() -> Self {
+        Self {
+            aspect_ratio: 16.0 / 9.0,
+            image_width: 100,
+
+            // Derived
+            image_height: 0,
+            origin: Point3::zeros(),
+            pixel00_loc: Point3::zeros(),
+            pixel_delta_u: Vec3::zeros(),
+            pixel_delta_v: Vec3::zeros(),
+        }
+    }
+}
+
+impl Camera {
+    pub fn render(&mut self, world: &impl Hittable) {
+        self.initialize();
+
+        println!("P3\n{} {}\n{}", self.image_width, self.image_height, 255);
+
+        for j in 0..self.image_height {
+            for i in 0..self.image_width {
+                let pixel_center = self.pixel00_loc
+                    + (i as f64 * self.pixel_delta_u)
+                    + (j as f64 * self.pixel_delta_v);
+                let ray_direction = pixel_center - self.origin;
+                let r = Ray::new(self.origin, ray_direction);
+
+                let pixel_color = Camera::ray_color(&r, world);
+                println!("{}", pixel_color.write_color());
+            }
+
+            eprint!(
+                "\rRendering Output: {:.1}% completed",
+                (j as f64 / (self.image_height - 1) as f64) * 100.0
+            );
+        }
+
+        eprintln!("\rDone.                                  \n");
+    }
+
+    fn initialize(&mut self) {
+        self.image_height = (self.image_width as f64 / self.aspect_ratio) as i32;
+
+        let viewport_height = 2.0;
+        let viewport_width = viewport_height * (self.image_width as f64 / self.image_height as f64);
+
+        let focal_length = 1.0;
+        let camera_origin = Vec3::zeros();
+
+        // Viewport Vectors
+        let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
+        let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
+
+        self.pixel_delta_u = viewport_u / self.image_width as f64;
+        self.pixel_delta_v = viewport_v / self.image_height as f64;
+
+        let viewport_upper_left =
+            camera_origin - Vec3::new(0.0, 0.0, focal_length) - viewport_u / 2.0 - viewport_v / 2.0;
+        self.pixel00_loc = viewport_upper_left + (self.pixel_delta_u + self.pixel_delta_v) / 2.0;
+    }
+
+    fn ray_color(r: &Ray, world: &impl Hittable) -> Color {
+        let mut rec = HitRecord::empty();
+        if world.hit(r, Interval::right_open(0.0), &mut rec) {
+            return 0.5 * (rec.normal + Color::new(1.0, 1.0, 1.0));
+        }
+
+        let unit_direction = r.direction();
+        let t = 0.5 * (unit_direction.y() + 1.0);
+        Color::new(1.0, 1.0, 1.0) * (1.0 - t) + Color::new(0.5, 0.7, 1.0) * t
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod camera;
 mod color;
 mod hittable;
 mod interval;
@@ -5,50 +6,13 @@ mod ray;
 mod sphere;
 mod vec3;
 
+use crate::camera::Camera;
 use crate::color::ColorWriter;
-use crate::hittable::{HitRecord, Hittable, HittableList};
-use crate::interval::Interval;
-use crate::ray::Ray;
+use crate::hittable::{Hittable, HittableList};
 use crate::sphere::Sphere;
-use crate::vec3::{Color, Point3, Vec3, XYZAccessor};
-
-fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> f64 {
-    let oc = r.origin() - center;
-    let a = 1.0;
-    let half_b = oc.dot(&r.direction());
-    let c = oc.dot(&oc) - radius * radius;
-    let discriminant = half_b * half_b - a * c;
-
-    if discriminant < 0.0 {
-        -1.0
-    } else {
-        (-half_b - discriminant.sqrt()) / a
-    }
-}
-
-fn ray_color(r: &Ray, world: &impl Hittable) -> Color {
-    let mut rec = HitRecord::empty();
-    if world.hit(r, Interval::right_open(0.0), &mut rec) {
-        return 0.5 * (rec.normal + Color::new(1.0, 1.0, 1.0));
-    }
-
-    let unit_direction = r.direction();
-    let t = 0.5 * (unit_direction.y() + 1.0);
-    Color::new(1.0, 1.0, 1.0) * (1.0 - t) + Color::new(0.5, 0.7, 1.0) * t
-}
+use crate::vec3::{Point3, XYZAccessor};
 
 fn main() {
-    // Image
-
-    let aspect_ratio = 16.0 / 9.0;
-    let image_width = 1080;
-    let image_height = (image_width as f64 / aspect_ratio) as i32;
-
-    eprintln!("Image Width: {}, Height: {}", image_width, image_height);
-
-    let viewport_height = 2.0;
-    let viewport_width = viewport_height * (image_width as f64 / image_height as f64);
-
     // World
 
     let mut world = HittableList::new();
@@ -58,38 +22,8 @@ fn main() {
 
     // Camera
 
-    let focal_length = 1.0;
-    let camera_origin = Vec3::zeros();
+    let mut cam = Camera::new();
+    cam.image_width = 400;
 
-    // Viewport Vectors
-    let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
-    let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
-
-    let pixel_delta_u = viewport_u / image_width as f64;
-    let pixel_delta_v = viewport_v / image_height as f64;
-
-    let viewport_upper_left =
-        camera_origin - Vec3::new(0.0, 0.0, focal_length) - viewport_u / 2.0 - viewport_v / 2.0;
-    let pixel00_loc = viewport_upper_left + (pixel_delta_u + pixel_delta_v) / 2.0;
-
-    println!("P3\n{} {}\n{}", image_width, image_height, 255);
-
-    for j in 0..image_height {
-        for i in 0..image_width {
-            let pixel_center =
-                pixel00_loc + (i as f64 * pixel_delta_u) + (j as f64 * pixel_delta_v);
-            let ray_direction = pixel_center - camera_origin;
-            let r = Ray::new(camera_origin, ray_direction);
-
-            let pixel_color = ray_color(&r, &world);
-            println!("{}", pixel_color.write_color());
-        }
-
-        eprint!(
-            "\rRendering Output: {:.1}% completed",
-            (j as f64 / (image_height - 1) as f64) * 100.0
-        );
-    }
-
-    eprintln!("\nDone!");
+    cam.render(&world);
 }
