d0fd9143d1e3a0a6d73c05e31570486bd27c2f23
gserifi <gent.serifi@gmail.com>
2024-02-11 20:01:07 +0100

    adding surface normal shading

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod color;
+mod hittable;
 mod ray;
 mod vec3;
 
@@ -6,24 +7,30 @@ use crate::color::ColorWriter;
 use crate::ray::Ray;
 use crate::vec3::{Color, Point3, Vec3, XYZAccessor};
 
-fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> bool {
+fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> f64 {
     let oc = r.origin() - center;
     let a = 1.0;
-    let b = 2.0 * oc.dot(&r.direction());
+    let half_b = oc.dot(&r.direction());
     let c = oc.dot(&oc) - radius * radius;
-    let discriminant = b * b - 4.0 * a * c;
+    let discriminant = half_b * half_b - a * c;
 
-    discriminant >= 0.0
+    if discriminant < 0.0 {
+        -1.0
+    } else {
+        (-half_b - discriminant.sqrt()) / a
+    }
 }
 
 fn ray_color(r: &Ray) -> Color {
-    if hit_sphere(&Point3::new(0.0, 0.0, -1.0), 0.5, r) {
-        return Color::new(1.0, 0.10588, 0.41960);
+    let t = hit_sphere(&Point3::new(0.0, 0.0, -1.0), 0.5, r);
+    if t > 0.0 {
+        let n = (r.at(t) - Vec3::new(0.0, 0.0, -1.0)).normalize();
+        return 0.5 * Color::new(n.x() + 1.0, n.y() + 1.0, n.z() + 1.0);
     }
 
     let unit_direction = r.direction();
     let t = 0.5 * (unit_direction.y() + 1.0);
-    Color::new(0.0, 0.38039, 1.0) * (1.0 - t) + Color::new(0.37647, 0.93725, 1.0) * t
+    Color::new(1.0, 1.0, 1.0) * (1.0 - t) + Color::new(0.5, 0.7, 1.0) * t
 }
 
 fn write_ppm(w: i32, h: i32) {
@@ -41,7 +48,7 @@ fn main() {
     // Image
 
     let aspect_ratio = 16.0 / 9.0;
-    let image_width = 400;
+    let image_width = 720;
     let image_height = (image_width as f64 / aspect_ratio) as i32;
 
     let viewport_height = 2.0;
